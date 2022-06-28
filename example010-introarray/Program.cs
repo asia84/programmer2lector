8d33b6694c6f8c2b45890a2cddbc5f586c3a7cd8
Asia84 <84asia@mail.ru>
2022-06-28 19:49:53 +0700

    задача ex.010 - нахождение заднного значения

---
+++ example010-introarray/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = { 14, 21, 43, 41, 53, 60, 17, 48, 29 };
+
+int n = array.Length;
+int find = 43;
+
+
+int index = 0;
+
+while (index < n)
+{
+
+    if (array[index] == find)
+    {
+        Console. WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
