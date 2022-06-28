d4a36cc23c973bbe71e2155342d95ef8e43dca04
Asia84 <84asia@mail.ru>
2022-06-28 19:27:03 +0700

    задача10-нахождение индекса числа из массива

---
+++ урок10/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = { 14, 21, 43, 41, 53, 60, 17, 21, 29 };
+
+int n = array.Length;
+int find = 21;
+
+
+int index = 0;
+
+while (index < n)
+{
+
+    if (array[index] == find)
+    {
+        Console. WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
